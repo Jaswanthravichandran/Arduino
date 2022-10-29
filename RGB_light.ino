26e400a500ec7e1e0b33a7fc31cda58fb8be1b84
Jaswanthravichandran <97626198+Jaswanthravichandran@users.noreply.github.com>
2022-10-29 15:15:02 +0530

    Create RGB_light.ino

---
+++ RGB_light.ino
@@ -0,0 +1,25 @@
+const int red = 9;
+const int green = 10;
+const int blue = 11;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop() {
+  analogWrite(red,255);
+  analogWrite(green,0);
+  analogWrite(blue,0);
+  delay(200);
+  analogWrite(red,0);
+  analogWrite(green,255);
+  analogWrite(blue,0);
+  delay(200);
+  analogWrite(red,0);
+  analogWrite(green,0);
+  analogWrite(blue,255);
+  delay(200);
+    
+}
